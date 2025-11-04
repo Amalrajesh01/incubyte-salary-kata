0fb86bbba2f1b43153641242b5ba0591f473036a
amalrajesh01 <amalrajesh01@gmail.com>
2025-11-04 12:54:39 +0530

    feat(EmployeeController): implement POST /employees endpoint to pass test

---
+++ src/main/java/com/incubyte/controller/EmployeeController.java
@@ -0,0 +1,24 @@
+package com.incubyte.controller;
+
+import com.incubyte.model.Employee;
+import com.incubyte.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee) {
+        Employee savedEmployee = employeeService.createEmployee(employee);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedEmployee);
+    }
+}
