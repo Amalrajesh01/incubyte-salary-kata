355d97a15836f630577f355fcb3d87135efb3981
amalrajesh01 <amalrajesh01@gmail.com>
2025-11-04 07:20:37 +0530

    initialize Spring Boot project for Salary Management API

---
+++ src/main/java/com/incubyte/IncubyteSalaryKataApplication.java
@@ -0,0 +1,13 @@
+package com.incubyte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncubyteSalaryKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncubyteSalaryKataApplication.class, args);
+	}
+
+}
